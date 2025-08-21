485e7708a341f66f364982abe01cc727d34ed252
Vasilii Glebov <v.glebov@insentry.io>
2025-08-21 22:46:55 +0300

    cut off the extra

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+
+import * as React from 'react';
+
+export const Form: React.ComponentType<any>;
+export const FormProvider: React.ComponentType<any>;
+export const Condition: React.ComponentType<any>;
+export const Field: any;
+export const fieldPresets: any;
+export function createField(...args: any[]): any;
+
+declare const _default: any;
+export default _default;
